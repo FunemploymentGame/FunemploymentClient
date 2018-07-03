05e949ba0ea0259d820b7de72eb14fc35a69bd95
Anthony Green <Anthonydgreen90@gmail.com>
2018-07-03 11:19:43 -0700

    re-added HomeController

---
+++ Funemployment/Funemployment/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Funemployment.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Funemployment.Controllers
+{
+    public class HomeController : Controller
+    {
+        private FunemploymentDbContext _context;
+
+        public HomeController(FunemploymentDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string username)
+        {
+            var player = _context.PlayerTable.FirstOrDefault(p => p.Username == username);
+            if (player != null)
+            {
+                return RedirectToAction("Index", "Profile", player);
+            }
+
+            return RedirectToAction("Create", "Profile");
+        }
+    }
+}
