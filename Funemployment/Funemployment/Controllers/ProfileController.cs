f138554e16ac4046b0b1b477123cdb26b19286c8
Calamario <mario.nishio@gmail.com>
2018-07-04 14:46:54 -0700

    view all answers have now worked - thanks max

--- Funemployment/Funemployment/Controllers/ProfileController.cs
+++ Funemployment/Funemployment/Controllers/ProfileController.cs
@@ -10,60 +10,60 @@ using Microsoft.Extensions.Configuration;
 
 namespace Funemployment.Controllers
 {
-	public class ProfileController : Controller
-	{
-		private FunemploymentDbContext _context;
-		private readonly IConfiguration Configuration;
+    public class ProfileController : Controller
+    {
+        private FunemploymentDbContext _context;
+        private readonly IConfiguration Configuration;
 
-		public ProfileController(FunemploymentDbContext context, IConfiguration configuration)
-		{
-			_context = context;
-			Configuration = configuration;
+        public ProfileController(FunemploymentDbContext context, IConfiguration configuration)
+        {
+            _context = context;
+            Configuration = configuration;
 
-		}
+        }
         /// <summary>
         /// Profile view
         /// </summary>
         /// <param name="player">The player</param>
         /// <returns>the view</returns>
 		public IActionResult Index(Player player)
-		{
-			return View(player);
-		}
+        {
+            return View(player);
+        }
 
         /// <summary>
         /// sends user to Create View form
         /// </summary>
         /// <returns>view</returns>
 		[HttpGet]
-		public IActionResult Create()
-		{
-			return View();
-		}
+        public IActionResult Create()
+        {
+            return View();
+        }
         /// <summary>
         /// Creates a new player with the user input data, saves to DB and saves ID to cookie
         /// </summary>
         /// <param name="player">A player</param>
         /// <returns>the profile index view</returns>
 		[HttpPost]
-		public async Task<IActionResult> Create(Player player)
-		{
-			await _context.PlayerTable.AddAsync(player);
-			await _context.SaveChangesAsync();
+        public async Task<IActionResult> Create(Player player)
+        {
+            await _context.PlayerTable.AddAsync(player);
+            await _context.SaveChangesAsync();
 
-			// Make sure that the player object is not null. If it is not, 
-			// then set the id of the player to the key/value pair ofthe cookie. 
-			if(player != null)
-			{
-				//Response is a part of hte Controller Base and is a part of the 
-				// HttpResposne object. 
-				// Append means we get to "add" a new cookie
-				Response.Cookies.Append("Player", player.ID.ToString());
-			}
-			return RedirectToAction("Index", player);
-		}
-    
-            /// <summary>
+            // Make sure that the player object is not null. If it is not, 
+            // then set the id of the player to the key/value pair ofthe cookie. 
+            if (player != null)
+            {
+                //Response is a part of hte Controller Base and is a part of the 
+                // HttpResposne object. 
+                // Append means we get to "add" a new cookie
+                Response.Cookies.Append("Player", player.ID.ToString());
+            }
+            return RedirectToAction("Index", player);
+        }
+
+        /// <summary>
         /// Show all users by points descending (highscore)
         /// </summary>
         /// <returns>view with list of players</returns>
@@ -83,28 +83,26 @@ namespace Funemployment.Controllers
             var player = _context.PlayerTable.FirstOrDefault(p => p.ID == id);
             return RedirectToAction("Index", "Profile", player);
         }
+
+        public async Task<IActionResult> PlayerAllAnswers(int id)
+        {
+            List<Answer> bQAnswers = _context.AnswerTable.Where(a => a.PID == id && a.BQID != 0).Select(s => s).ToList();
+
+            List<Answer> tQAnswers = _context.AnswerTable.Where(a => a.PID == id && a.TQID != 0).Select(s => s).ToList();
+
+            if (bQAnswers.Count != 0 && tQAnswers.Count != 0)
+            {
+                return View(await PlayerAllAnswersViewModel.FromPlayerIDAsync(id, _context, bQAnswers, tQAnswers));
+            }
+            else
+            {
+                return RedirectToAction("Error", "Profile", id);
+            }
+        }
+
+        public IActionResult Error(int id)
+        {
+            return View(id);
+        }
     }
-
-    public async Task<IActionResult> PlayerAllAnswers(int id)
-    {
-      List<Answer> bQAnswers = _context.AnswerTable.Where(a => a.PID == id && a.BQID != 0).Select(s => s).ToList();
-
-      List<Answer> tQAnswers = _context.AnswerTable.Where(a => a.PID == id && a.TQID != 0).Select(s => s).ToList();
-
-      if (bQAnswers.Count != 0 && tQAnswers.Count != 0)
-      {
-        return View(await PlayerAllAnswersViewModel.FromPlayerIDAsync(id, _context, bQAnswers, tQAnswers));
-      }
-      else
-      {
-        return RedirectToAction("Error", "Profile");
-      }
-    }
-
-    public IActionResult Error()
-    {
-      return View();
-    }
-	}
-
 }
